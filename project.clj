f8ec5dc5ea73321582054a46f4580633b6485c1f
Vít Kotačka <vit.kotacka@gmail.com>
2017-05-02 14:36:48 +0200

    Midje tests have been replaced with clojure.test.

--- project.clj
+++ project.clj
@@ -1,9 +1,7 @@
-(defproject blog-concurrency "0.0.1-SNAPSHOT"
+(defproject blog-concurrency "0.1.0-SNAPSHOT"
   :description "A trial Clojure project for a blogpost about concurrency."
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
-  :profiles {:dev {:dependencies [[midje "1.8.3"]]
-                   :plugins [[lein-midje "3.2.1"]]}})
+  :dependencies [[org.clojure/clojure "1.8.0"]])
 
   
--- test/blog_concurrency/var_test.clj
+++ test/blog_concurrency/var_test.clj
@@ -1,22 +1,22 @@
 (ns blog-concurrency.var-test
-  (:require [midje.sweet :refer :all]
+  (:require [clojure.test :refer :all]
             [blog-concurrency.var :refer :all]))
 
-(fact "static binding throws an exception"
-  (binding [static-var "re-binded var"] static-var) =>
-      (throws IllegalStateException))
+(deftest test-static-var-binding
+  (testing "static binding throws an exception"
+    (is (thrown? IllegalStateException
+                 (binding [static-var "re-binded var"] static-var)))))
 
-(fact "dynamic var can be re-binded"
-  re-binded-var => "re-binded var")
+(deftest test-dynamic-var-binding
+  (testing "dynamic var can be re-binded"
+    (is (= "re-binded var" re-binded-var)))
+  (testing "root binding is still untouched"
+    (is (= "dynamic var" *dynamic-var*)))
+  (testing "vars are global"
+    (is (= "future var" future-var))))
 
-(fact "root binding is still untouched"
-  *dynamic-var* => "dynamic var")
-
-(fact "vars are global"
-  future-var => "future var")
-
-(fact "The Answer to the Ultimate Question of Life, The Universe, and Everything"
-  (compute) => 42)
-
-(fact "the answer in binary form"
-  (compute-binary) => "101001")
+(deftest test-dynamic-fn-binding
+  (testing "The Answer to the Ultimate Question of Life, The Universe, and Everything"
+    (is (= 42 (compute))))
+  (testing "the answer in binary form"
+    (is (= "101001" (compute-binary)))))
